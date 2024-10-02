eff8268b12821a0157b11fe619d58bca2423d88c
Sergey Stupak <itstupak@gmail.com>
2024-10-02 16:42:43 +0200

    update

--- src/server.js
+++ src/server.js
@@ -14,22 +14,35 @@
 // Не забудьте вказати змінну оточення в файлі .env.example
 
 import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+import { env } from './utils/env.js';
+
+// Читаємо змінну оточення PORT
+const PORT = Number(env('PORT', '3000'));
 
 export function setupServer() {
 
   const app = express();
-  const PORT = 3000;
+  app.use(express.json()); // Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах, наприклад, у запитах POST або PATCH
+  app.use(cors());
 
+  //Pino надає нам middleware і можливість додатково налаштовувати логгер через об’єкт властивостей.
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+/*
   // Middleware для логування часу запиту
   app.use((req, res, next) => {
     console.log(`Time: ${new Date().toLocaleString()}`);
     next();
   });
-
-  // Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах
-  // наприклад, у запитах POST або PATCH
-  app.use(express.json());
-
+ */
   // Маршрут для обробки GET-запитів на '/'
   app.get('/', (req, res) => {
     res.json({
@@ -37,10 +50,17 @@ export function setupServer() {
     });
   });
 
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
   // Middleware для обробких помилок (приймає 4 аргументи)
   app.use((err, req, res, next) => {
     res.status(500).json({
       message: 'Something went wrong',
+      error: err.message,
     });
   });
 
