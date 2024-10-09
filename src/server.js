532fcc4a9bbaf1bb9eb48ddc7ed0c8883931b6d9
Sergey Stupak <itstupak@gmail.com>
2024-10-09 13:13:05 +0200

    created routers & controllers

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import contactsRouter from './routers/contacts.js'; // Імпортуємо роутер
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '8080'));
 
@@ -20,36 +20,7 @@ export function setupServer() {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-
-    res.status(200).json({
-      status: 200,
-      message: "Successfully found contacts!",
-      data: contacts,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    // Відповідь, якщо контакт не знайдено
-	if (!contact) {
-    res.status(404).json({
-      status: 404,
-		  message: 'Contact not found'
-	  });
-	  return;
-	}
-
-	// Відповідь, якщо контакт знайдено
-    res.status(200).json({
-      status: 200,
-	    message: `Successfully found contact with id: ${contactId}!`,
-      data: contact,
-    });
-  });
+  app.use(contactsRouter); // Додаємо роутер до app як middleware
 
   // Middleware для обробких status 404
   app.use('*', (req, res, next) => {
