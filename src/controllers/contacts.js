d0393160c573105c27b3936c321452384bda9ead
Sergey Stupak <itstupak@gmail.com>
2024-10-10 11:03:41 +0200

    added POST router

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,30 +1,40 @@
-import { getAllContacts, getContactById } from '../services/contacts.js';
+import { createContact, getAllContacts, getContactById } from '../services/contacts.js';
 import createHttpError from 'http-errors';
 
 
 export const getContactsController = async (req, res) => {
-    const contacts = await getAllContacts();
+  const contacts = await getAllContacts();
 
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  };
+  res.json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
 
-  export const getContactByIdController = async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
+export const getContactByIdController = async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
 
-    // Відповідь, якщо контакт не знайдено
-    if (!contact) {
-      throw createHttpError(404, 'Contact not found');
-    }
+  // Відповідь, якщо контакт не знайдено
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
 
-    // Відповідь, якщо контакт знайдено
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  };
+  // Відповідь, якщо контакт знайдено
+  res.json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
+
+export const createContactController = async (req, res) => {
+  const contact = await createContact(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: `Successfully created a contact!`,
+    data: contact,
+  });
+};
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,8 @@ export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
   return contact;
 };
+
+export const createContact = async (doc) => {
+  const contact = await ContactsCollection.create(doc);
+  return contact;
+};
