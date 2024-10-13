f1834ea603618293e336e73056190c0678f06acb
Sergey Stupak <itstupak@gmail.com>
2024-10-14 01:55:07 +0200

    update final version

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -31,41 +31,39 @@ export const getContactByIdController = async (req, res) => {
 
 
 export const createContactController = async (req, res) => {
-  const contact = await createContact(req.body);
+  const newContact = await createContact(req.body);
 
   res.status(201).json({
     status: 201,
     message: `Successfully created a contact!`,
-    data: contact,
+    data: newContact,
   });
 };
 
 
-export const deleteContactController = async (req, res, next) => {
+export const deleteContactController = async (req, res) => {
   const { contactId } = req.params;
   const contact = await deleteContact(contactId);
 
   if (!contact) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.status(204).send();
 };
 
 
-export const patchContactController = async (req, res, next) => {
+export const patchContactController = async (req, res) => {
   const { contactId } = req.params;
   const result = await updateContact(contactId, req.body);
 
   if (!result) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
-  res.json({
+  res.status(200).json({
     status: 200,
     message: `Successfully patched a Contact!`,
-    data: result.contact,
+    data: result,
   });
 };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,42 +1,21 @@
 import { ContactsCollection } from '../db/models/contacts.js';
 
-export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
+export const getAllContacts = () => {
+  return ContactsCollection.find();
 };
 
-export const getContactById = async (contactId) => {
-  const contact = await ContactsCollection.findById(contactId);
-  return contact;
+export const getContactById = (contactId) => {
+  return ContactsCollection.findById(contactId);
 };
 
-export const createContact = async (doc) => {
-  const contact = await ContactsCollection.create(doc);
-  return contact;
+export const createContact = (payload) => {
+  return ContactsCollection.create(payload);
 };
 
-export const deleteContact = async (contactId) => {
-  const contact = await ContactsCollection.findOneAndDelete({
-    _id: contactId,
-  });
-  return contact;
+export const deleteContact = (contactId) => {
+  return ContactsCollection.findOneAndDelete({ _id: contactId });
 };
 
-export const updateContact = async (contactId, payload, options = {}) => {
-  const rawResult = await ContactsCollection.findOneAndUpdate(
-    { _id: contactId },
-    payload,
-    {
-      new: true,
-      includeResultMetadata: true,
-      ...options,
-    },
-  );
-
-  if (!rawResult || !rawResult.value) return null;
-
-  return {
-    student: rawResult.value,
-    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
-  };
+export const updateContact = (contactId, payload) => {
+  return ContactsCollection.findOneAndUpdate({ _id: contactId }, payload, { new: true });
 };
