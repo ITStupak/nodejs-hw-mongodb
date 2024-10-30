2397922840a8e773362025f8e7d1052f3f75fb17
Sergey Stupak <itstupak@gmail.com>
2024-10-30 01:31:54 +0100

    delete roles

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -15,19 +15,16 @@ import { createContactSchema, updateContactSchema } from '../validation/contacts
 import { isValidId } from '../middlewares/isValidId.js';
 
 import { authenticate } from '../middlewares/authenticate.js';
-import { checkRoles } from '../middlewares/checkRoles.js';
-import { ROLES } from '../constants/index.js';
-
 
 const router = Router();
 const jsonParser = express.json();
 
 router.use(authenticate);
 
-router.get('/', checkRoles(ROLES.ADMIN), ctrlWrapper(getContactsController));
-router.get('/:contactId', isValidId, checkRoles(ROLES.ADMIN, ROLES.PERSON), ctrlWrapper(getContactByIdController));
-router.post('/', jsonParser, validateBody(createContactSchema), checkRoles(ROLES.ADMIN), ctrlWrapper(createContactController));
-router.delete('/:contactId', isValidId, checkRoles(ROLES.ADMIN), ctrlWrapper(deleteContactController));
-router.patch('/:contactId', jsonParser, isValidId, checkRoles(ROLES.ADMIN, ROLES.PERSON), validateBody(updateContactSchema), ctrlWrapper(patchContactController));
+router.get('/', ctrlWrapper(getContactsController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+router.post('/', jsonParser, validateBody(createContactSchema), ctrlWrapper(createContactController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
+router.patch('/:contactId', jsonParser, isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 
 export default router;
